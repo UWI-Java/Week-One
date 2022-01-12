6d22ab36b4d2e2752f927e016d4560684814ea6d
AnnabelleEllis <annabelleellis1999@gmail.com>
2022-01-11 22:48:30 -0500

    Pre-Lab Excercise Two

---
+++ src/com/annabelle/java/prelab/Poem.java
@@ -0,0 +1,11 @@
+package com.annabelle.java.prelab;
+
+public class Poem {
+    public static void main(String[] args) {
+        System.out.println("""
+                Roses are red
+                Violets are blue
+                Sugar is sweet
+                And so are you!""");
+    }
+}
